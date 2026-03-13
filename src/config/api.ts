5a8110ba5d9c6683a1ea95183b2cc9681ac1cd22
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-03-13 17:39:29 -0300

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 const DEFAULT_VERIFY_PASSWORD_URL = "/api/verify-password";
-const DEFAULT_UPLOAD_URL = "/api/upload";
+const DEFAULT_UPLOAD_URL = "/api/upload-url";
 
 export const VERIFY_PASSWORD_URL = (import.meta.env.VITE_VERIFY_PASSWORD_URL || DEFAULT_VERIFY_PASSWORD_URL).trim();
 export const UPLOAD_URL = (import.meta.env.VITE_UPLOAD_URL || DEFAULT_UPLOAD_URL).trim();
