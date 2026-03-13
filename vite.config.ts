d7ea401435f5187a83953ab13d4bd4968ed62a70
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-03-13 11:29:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,25 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash][extname]",
+      },
     },
   },
 });
